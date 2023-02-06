2425fa7b95f687f3bc02acf2a90d0eb7aa9e8306
JonG <2111778+jtgrenz@users.noreply.github.com>
2023-02-06 15:37:46 -0500

    Adds support for Hash-like objects
    When given an object that for all intents and purposes acts as a hash,
    to_dots should outout the correct string.
    
    Example: Rails 6.1 introduces a new class that uses SimpleDelegator to
    decorate the standard hash class called DeprecationHandlingDetailsHash.
    It adds a couple of instance variables and delegates everything else to
    Hash.
    
    Because it was not a Hash however, we would see something like this:
    
    `{'users' => DeprecationHandlingDetailsHash.new({:base=>{[:error=> :failed_to_update}]})}`
    
    Would serialize as ["user.{:base=>[{:error=>:failed_to_update}]}"] when
    we would expect ["user.base.error.failed_to_update"]

--- lib/to_dots.rb
+++ lib/to_dots.rb
@@ -4,14 +4,13 @@ require_relative './array'
 
 module ToDots
   def self.to_dots(object, result = [], prefix = '')
-    case object
-    when ::Hash
+    if object.respond_to?(:to_hash)
       object.each do |key, value|
         to_dots(value, result, "#{prefix}#{'.' unless prefix.empty?}#{key}")
       end
-    when ::Array
-      object.each_with_index do |value, index|
-        to_dots(value, result, "#{prefix}")
+    elsif object.is_a?(Array)
+      object.each do |value|
+        to_dots(value, result, prefix.to_s)
       end
     else
       result << "#{prefix}.#{object}"
